556dd738330f97d8640e3779092185a83698186f
tangwuyang <614270525@qq.com>
2018-02-05 20:47:48 +0800

    再次添加test文件

---
+++ app/src/main/java/com/anxin/kitchen/tangwuyangs_test/Test.java
@@ -0,0 +1,13 @@
+package com.anxin.kitchen.tangwuyangs_test;
+
+import com.anxin.kitchen.utils.Log;
+
+/**
+ * Created by 唐午阳 on 2018/2/5.
+ */
+
+public class Test {
+    public static void main(){
+        Log.d("mmm","sssss");
+    }
+}
