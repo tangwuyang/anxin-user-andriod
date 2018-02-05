532631d87edc7e43e34c2fb399f944fc891b9f59
guohua_z <614270525@qq.com>
2018-02-05 20:02:58 +0800

    准备测试地图

--- app/src/main/java/com/anxin/kitchen/fragment/myfragment/MyMainFragment.java
+++ app/src/main/java/com/anxin/kitchen/fragment/myfragment/MyMainFragment.java
@@ -1,9 +1,11 @@
 package com.anxin.kitchen.fragment.myfragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.anxin.kitchen.fragment.HomeBaseFragment;
 import com.anxin.kitchen.user.R;
@@ -15,7 +17,7 @@ import com.anxin.kitchen.utils.Log;
 public class MyMainFragment extends HomeBaseFragment implements View.OnClickListener {
     private Log LOG = Log.getLog();
     private View view;
-
+    private Button mTestMapBt;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +31,8 @@ public class MyMainFragment extends HomeBaseFragment implements View.OnClickList
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.my_main_fragment, null);
+        mTestMapBt = view.findViewById(R.id.test_map_bt);
+        mTestMapBt.setOnClickListener(this);
         initView();//初始化界面控制
         return view;
     }
@@ -45,6 +49,9 @@ public class MyMainFragment extends HomeBaseFragment implements View.OnClickList
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.test_map_bt:
+                Intent intent = new Intent(getContext(),TestMapActivity.class);
+                startActivity(intent);
             default:
                 break;
         }
---
+++ app/src/main/java/com/anxin/kitchen/tangwuyangs_test/TestMapActivity.java
@@ -0,0 +1,13 @@
+package com.anxin.kitchen.tangwuyangs_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_map2);
+    }
+}
