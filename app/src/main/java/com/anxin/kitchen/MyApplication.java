33a807fd3b487549b9ce069365e6587c4b034427
tangwuyang <614270525@qq.com>
2018-02-28 19:33:43 +0800

    新增application

--- app/src/main/java/com/anxin/kitchen/MyApplication.java
+++ app/src/main/java/com/anxin/kitchen/MyApplication.java
@@ -4,12 +4,18 @@ import android.app.Application;
 
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.UMShareAPI;
 
 /**
  * Created by 唐午阳 on 2018/2/26.
  */
 
 public class MyApplication extends Application {
+    {
+        PlatformConfig.setWeixin("wx967daebe835fbeac","5bb696d9ccd75a38c8a0bfe0675559b3");
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+    }
     @Override
     public void onCreate() {
         super.onCreate();
@@ -17,5 +23,7 @@ public class MyApplication extends Application {
         UMConfigure.setLogEnabled(true);
         UMConfigure.setEncryptEnabled(false);
         MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
+        UMShareAPI.get(this);
+
     }
 }
