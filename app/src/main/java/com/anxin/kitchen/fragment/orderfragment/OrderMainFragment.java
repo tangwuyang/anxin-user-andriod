c509fd107215228aa6c205005f23d9666507aa05
guohua_z <798918322@qq.com>
2018-02-28 17:14:53 +0800

    添加微信登陆接口

--- app/src/main/java/com/anxin/kitchen/fragment/orderfragment/OrderMainFragment.java
+++ app/src/main/java/com/anxin/kitchen/fragment/orderfragment/OrderMainFragment.java
@@ -2,15 +2,28 @@ package com.anxin.kitchen.fragment.orderfragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.anxin.kitchen.activity.AllOrderActivity;
+import com.anxin.kitchen.bean.OrderInfoBean;
 import com.anxin.kitchen.fragment.HomeBaseFragment;
 import com.anxin.kitchen.user.R;
 import com.anxin.kitchen.utils.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 订单主界面
@@ -20,6 +33,9 @@ public class OrderMainFragment extends HomeBaseFragment implements View.OnClickL
     private Log LOG = Log.getLog();
     private View view;
     private TextView mAllOrderBtn, mAllOrderBtn2;//全部订单
+    private List<OrderInfoBean> myOrderList = new ArrayList<>();//订单集合
+    private MyOrderAdaped myOrderAdaped;//订单适配器
+    private ListView myOrderListView;//订单列表
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,10 +58,17 @@ public class OrderMainFragment extends HomeBaseFragment implements View.OnClickL
      * 初始化控件，点击事件
      */
     private void initView() {
-        mAllOrderBtn = (TextView) view.findViewById(R.id.allOrder_btn);
-        mAllOrderBtn2 = (TextView) view.findViewById(R.id.allOrder_btn2);
+        mAllOrderBtn = (TextView) view.findViewById(R.id.allOrder_btn);//我的全部订单
+        mAllOrderBtn2 = (TextView) view.findViewById(R.id.allOrder_btn2);//我的全部订单
         mAllOrderBtn.setOnClickListener(this);
         mAllOrderBtn2.setOnClickListener(this);
+
+        //初始化ExpandlistView的id
+        myOrderListView = (ListView) view.findViewById(R.id.myorder_listview);
+        //初始化列表数据，正常由服务器返回的Json数据
+        initJsonData();
+        myOrderAdaped = new MyOrderAdaped();
+        myOrderListView.setAdapter(myOrderAdaped);
     }
 
     @Override
@@ -74,6 +97,50 @@ public class OrderMainFragment extends HomeBaseFragment implements View.OnClickL
      */
     private void initJsonData() {
 
+
+    }
+
+    /**
+     * 最近订单适配器
+     */
+    class MyOrderAdaped extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return myOrderList.size();
+        }
+
+        @Override
+        public OrderInfoBean getItem(int position) {
+            // TODO Auto-generated method stub
+            return myOrderList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+
+            View view = convertView;
+
+            final ViewHolder holder;
+            if (view == null) {
+                holder = new ViewHolder();
+                view = LayoutInflater.from(getActivity()).inflate(R.layout.order_item_center, null);
+                view.setTag(holder);
+            } else {
+                holder = (ViewHolder) view.getTag();
+            }
+            return view;
+        }
+
+        class ViewHolder {
+        }
 
     }
 }
---
+++ app/src/main/java/com/anxin/kitchen/user/wxapi/WXEntryActivity.java
@@ -0,0 +1,194 @@
+package com.anxin.kitchen.user.wxapi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.anxin.kitchen.utils.Log;
+import com.anxin.kitchen.utils.SystemUtility;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.MySSLSocketFactory;
+import com.tencent.mm.opensdk.constants.ConstantsAPI;
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.modelmsg.SendAuth;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * 微信登录页面
+ *
+ * @author kevin_chen 2016-12-10 下午19:03:45
+ * @version v1.0
+ */
+public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
+    private Log LOG = Log.getLog();
+    private static final String APP_SECRET = "75bf85878994d867fd4bcec28ccca5aa";
+    private IWXAPI mWeixinAPI;
+    public static final String WEIXIN_APP_ID = "wx634128d6db8c15cd";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mWeixinAPI = WXAPIFactory.createWXAPI(this, WEIXIN_APP_ID, true);
+        mWeixinAPI.handleIntent(this.getIntent(), this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        mWeixinAPI.handleIntent(intent, this);//必须调用此句话
+    }
+
+    //微信发送的请求将回调到onReq方法
+    @Override
+    public void onReq(BaseReq req) {
+        LOG.e("-----onReq");
+        switch (req.getType()) {
+            case ConstantsAPI.COMMAND_GETMESSAGE_FROM_WX:
+                LOG.e("-------------COMMAND_GETMESSAGE_FROM_WX");
+                break;
+            case ConstantsAPI.COMMAND_SHOWMESSAGE_FROM_WX:
+                LOG.e("-------------COMMAND_SHOWMESSAGE_FROM_WX");
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    //发送到微信请求的响应结果
+    @Override
+    public void onResp(BaseResp resp) {
+        LOG.e("-------------onResp");
+        LOG.e("-------------errCode"+resp.errCode);
+        switch (resp.errCode) {
+            case BaseResp.ErrCode.ERR_OK:
+                LOG.e("-------ERR_OK");
+                //发送成功
+                SendAuth.Resp sendResp = (SendAuth.Resp) resp;
+                if (sendResp != null) {
+                    String code = sendResp.code;
+                    getAccess_token(code);
+                }
+                break;
+            case BaseResp.ErrCode.ERR_USER_CANCEL:
+                LOG.e("---------ERR_USER_CANCEL");
+                //发送取消
+                break;
+            case BaseResp.ErrCode.ERR_AUTH_DENIED:
+                LOG.e("------------ERR_AUTH_DENIED");
+                //发送被拒绝
+                break;
+            default:
+                //发送返回
+                LOG.e("------------发送返回");
+                break;
+        }
+//        finish();
+    }
+
+    /**
+     * 获取openid accessToken值用于后期操作
+     *
+     * @param code 请求码
+     */
+    private void getAccess_token(final String code) {
+        String path = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="
+                + WEIXIN_APP_ID
+                + "&secret="
+                + APP_SECRET
+                + "&code="
+                + code
+                + "&grant_type=authorization_code";
+        LOG.e("----getAccess_token：" + path);
+        //网络请求，根据自己的请求方式
+        AsyncHttpClient client = new AsyncHttpClient();
+        MySSLSocketFactory sf = SystemUtility.getMySSLSocketFactory();
+        client.setURLEncodingEnabled(true);
+        client.setSSLSocketFactory(sf);
+        client.get(path, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String result = "";
+                if (responseBody != null)
+                    result = new String(responseBody);
+                LOG.e("-------getAccess_token_result:" + result);
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(result);
+                    String openid = jsonObject.getString("openid").toString().trim();
+                    String access_token = jsonObject.getString("access_token").toString().trim();
+                    getUserMesg(access_token, openid);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable
+                    error) {
+
+            }
+        });
+    }
+
+
+    /**
+     * 获取微信的个人信息
+     *
+     * @param access_token
+     * @param openid
+     */
+    private void getUserMesg(final String access_token, final String openid) {
+        String path = "https://api.weixin.qq.com/sns/userinfo?access_token="
+                + access_token
+                + "&openid="
+                + openid;
+        LOG.e("----------getUserMesg：" + path);
+        //网络请求，根据自己的请求方式
+        AsyncHttpClient client = new AsyncHttpClient();
+        MySSLSocketFactory sf = SystemUtility.getMySSLSocketFactory();
+        client.setURLEncodingEnabled(true);
+        client.setSSLSocketFactory(sf);
+        client.get(path, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String result = "";
+                if (responseBody != null)
+                    result = new String(responseBody);
+                LOG.e("--------getUserMesg_result:" + result);
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(result);
+                    String nickname = jsonObject.getString("nickname");
+                    int sex = Integer.parseInt(jsonObject.get("sex").toString());
+                    String headimgurl = jsonObject.getString("headimgurl");
+
+                    LOG.e("用户基本信息:");
+                    LOG.e("nickname:" + nickname);
+                    LOG.e("sex:" + sex);
+                    LOG.e("headimgurl:" + headimgurl);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                finish();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable
+                    error) {
+
+            }
+        });
+    }
+
+}
