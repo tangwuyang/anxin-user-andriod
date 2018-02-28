7526fb65b8f5a9299aed818af438ba7def137723
tangwuyang <614270525@qq.com>
2018-02-28 22:11:52 +0800

    删除jar包添加keystore

--- app/src/main/java/com/anxin/kitchen/MyApplication.java
+++ app/src/main/java/com/anxin/kitchen/MyApplication.java
@@ -1,9 +1,13 @@
 package com.anxin.kitchen;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
+
 
 import com.umeng.analytics.MobclickAgent;
+
 import com.umeng.commonsdk.UMConfigure;
+import com.umeng.socialize.Config;
 import com.umeng.socialize.PlatformConfig;
 import com.umeng.socialize.UMShareAPI;
 
@@ -11,8 +15,8 @@ import com.umeng.socialize.UMShareAPI;
  * Created by 唐午阳 on 2018/2/26.
  */
 
-public class MyApplication extends Application {
-    {
+public class MyApplication extends MultiDexApplication {
+    {   Config.DEBUG = true;
         PlatformConfig.setWeixin("wx634128d6db8c15cd","75bf85878994d867fd4bcec28ccca5aa");
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");  //还未获取到
 
@@ -20,9 +24,11 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Config.DEBUG = true;
         UMConfigure.init(this,UMConfigure.DEVICE_TYPE_PHONE,"5a940df18f4a9d7d7b000197");
         UMConfigure.setLogEnabled(true);
-        UMConfigure.setEncryptEnabled(false);
+        PlatformConfig.setWeixin("wx634128d6db8c15cd","75bf85878994d867fd4bcec28ccca5aa");
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");  //还未获取到
         MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
         UMShareAPI.get(this);
     }
--- app/src/main/java/com/anxin/kitchen/activity/InvateFriendActivity.java
+++ app/src/main/java/com/anxin/kitchen/activity/InvateFriendActivity.java
@@ -1,6 +1,10 @@
 package com.anxin.kitchen.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Build.VERSION;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,9 +14,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.anxin.kitchen.user.R;
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMAuthListener;
 import com.umeng.socialize.UMShareAPI;
 import com.umeng.socialize.UMShareListener;
 import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.media.UMImage;
+
+import java.util.Map;
+
+import static android.os.Build.VERSION.*;
 
 public class InvateFriendActivity extends BaseActivity implements View.OnClickListener{
     private ImageView mBackImg;
@@ -56,12 +67,47 @@ public class InvateFriendActivity extends BaseActivity implements View.OnClickLi
 
         }
     };
+
+    UMAuthListener authListener = new UMAuthListener() {
+        @Override
+        public void onStart(SHARE_MEDIA platform) {
+            //授权开始的回调，可以用来处理等待框，或相关的文字提示
+        }
+
+        @Override
+        public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
+
+        }
+
+        @Override
+        public void onError(SHARE_MEDIA platform, int action, Throwable t) {
+        }
+
+        @Override
+        public void onCancel(SHARE_MEDIA platform, int action) {
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invate_friend_activity);
+        requestPermission();
         initView();
     }
+
+
+    private void requestPermission(){
+        String[] mPermissionList =
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.CALL_PHONE,Manifest.permission.READ_LOGS,
+                        Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.SET_DEBUG_APP,Manifest.permission.SYSTEM_ALERT_WINDOW,
+                        Manifest.permission.GET_ACCOUNTS,Manifest.permission.WRITE_APN_SETTINGS};
+        ActivityCompat.requestPermissions(this,mPermissionList,123);
+    }
+
 
     @Override
     protected void onResume() {
@@ -71,12 +117,17 @@ public class InvateFriendActivity extends BaseActivity implements View.OnClickLi
 
     private void initListeners() {
         mBackImg.setOnClickListener(this);
+        mShareQQLl.setOnClickListener(this);
+        mShareFriendGroupLl.setOnClickListener(this);
+        mShareWechatLl.setOnClickListener(this);
     }
 
     private void initView() {
         setTitle("邀请团友");
         mBackImg = findViewById(R.id.back_img);
-
+        mShareWechatLl = findViewById(R.id.share_wechat_ll);
+        mShareFriendGroupLl = findViewById(R.id.share_group_ll);
+        mShareQQLl = findViewById(R.id.share_qq_ll);
     }
 
     @Override
@@ -85,13 +136,53 @@ public class InvateFriendActivity extends BaseActivity implements View.OnClickLi
             case R.id.back_img:
                 onBackPressed();
                 break;
+            case R.id.share_wechat_ll:
+                shareWechatFriend();
+                break;
+
+            case R.id.share_qq_ll:
+                new ShareAction(InvateFriendActivity.this).setDisplayList(SHARE_MEDIA.WEIXIN)
+                        .withText("hello").setCallback(new UMShareListener() {
+                    @Override
+                    public void onStart(SHARE_MEDIA share_media) {
+
+                    }
+
+                    @Override
+                    public void onResult(SHARE_MEDIA share_media) {
+
+                    }
+
+                    @Override
+                    public void onError(SHARE_MEDIA share_media, Throwable throwable) {
+
+                    }
+
+                    @Override
+                    public void onCancel(SHARE_MEDIA share_media) {
+
+                    }
+                }).open();
+                break;
         }
+    }
+
+
+    //分享到微信好友
+    private void shareWechatFriend() {
+        UMImage image = new UMImage(InvateFriendActivity.this, R.drawable.search_bg);//资源文件
+        new ShareAction(InvateFriendActivity.this)
+                .setPlatform(SHARE_MEDIA.WEIXIN)//传入平台
+                .withText("hello")//分享内容
+                .withMedia(image)
+                .setCallback(shareListener)//回调监听器
+                .share();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
+
         UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
     }
 }
