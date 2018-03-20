536024bc2c4b90942dabacf7ef4dcf96d7c29560
guohua_z <798918322@qq.com>
2018-03-20 10:49:25 +0800

    添加用户性别设置

---
+++ app/src/main/java/com/anxin/kitchen/custom/view/SelectGenderPopupWindow.java
@@ -0,0 +1,48 @@
+package com.anxin.kitchen.custom.view;
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import com.anxin.kitchen.user.R;
+
+
+public class SelectGenderPopupWindow extends PopupWindow {
+
+
+    private TextView btn_take_photo, btn_pick_photo, btn_cancel;
+    private View mMenuView;
+
+    public SelectGenderPopupWindow(Context context, OnClickListener itemsOnClick) {
+        super(context);
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mMenuView = inflater.inflate(R.layout.fragment_headicon_dialog, null);
+        btn_take_photo = (TextView) mMenuView.findViewById(R.id.btn_take_photo);
+        btn_pick_photo = (TextView) mMenuView.findViewById(R.id.btn_pick_photo);
+        btn_take_photo.setText("男");
+        btn_pick_photo.setText("女");
+        btn_cancel = (TextView) mMenuView.findViewById(R.id.btn_cancel);
+        btn_cancel.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                SelectGenderPopupWindow.this.dismiss();
+            }
+        });
+        btn_pick_photo.setOnClickListener(itemsOnClick);
+        btn_take_photo.setOnClickListener(itemsOnClick);
+        this.setContentView(mMenuView);
+        this.setWidth(LayoutParams.FILL_PARENT);
+        this.setHeight(LayoutParams.MATCH_PARENT);
+        this.setFocusable(true);
+        this.setAnimationStyle(R.style.AnimBottom);
+        ColorDrawable dw = new ColorDrawable(context.getResources().getColor(R.color.dialog_background));
+        this.setBackgroundDrawable(dw);
+    }
+
+}
